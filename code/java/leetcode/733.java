a8baf7bd78b819c91a0cbbf753aeb6e3e35d2b73
정여진 <128468322+jnyn0314@users.noreply.github.com>
2026-09-25 23:38:59 +0900

    Implement flood fill solution

--- code/java/leetcode/733.java
+++ code/java/leetcode/733.java
@@ -1,5 +1,30 @@
 public class 733 {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        
+        boolean[][] visited = new boolean[image.length][image[0].length];
+        int originalColor = image[sr][sc];
+        dfs(image, sr, sc, color, visited, originalColor);
+
+        return image;
+    }
+    private void dfs(int[][] image, int sr, int sc, int color, boolean[][] visited, int originalColor){
+        if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length){
+            return;
+        }
+
+        if(visited[sr][sc]){
+            return;
+        }
+
+        if(image[sr][sc] != originalColor){
+            return;
+        }
+
+        visited[sr][sc] = true;
+        image[sr][sc] = color; // if문 밖이니까. 
+
+        dfs(image, r-1, c, color, visited, originalColor);
+        dfs(image, r, c-1, color, visited, originalColor);
+        dfs(image, r+1, c, color, visited, originalColor);
+        dfs(image, r, c+1, color, visited, originalColor);
     }
 }
