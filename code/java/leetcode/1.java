0dc3d7aa38bfa1625661e962fbc5d9a36db03e52
정여진 <128468322+jnyn0314@users.noreply.github.com>
2026-09-26 21:46:58 +0900

    Add two sum solutions

--- code/java/leetcode/1.java
+++ code/java/leetcode/1.java
@@ -19,3 +19,37 @@ class Solution {
         return answer;
     }
 }
+
+// 그냥 푸는 법
+class 1{
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    answer[0] = i;
+                    answer[1] = j;
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+}
+
+class 1{
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> hashmap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int need = target - nums[i];
+
+            if(hashmap.containsKey(need)){
+                return new int[]{hashmap.get(need), i};
+            }
+
+            hashmap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
